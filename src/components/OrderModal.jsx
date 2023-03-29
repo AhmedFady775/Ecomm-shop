e1e3600fb9e9a8091fc99970f6832d82f5215531
AhmedFady775 <fadiahmed298@gmail.com>
2023-03-30 01:11:28 +0200

    Adding new props to make the competent reusable

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -13,7 +13,9 @@ export default function OrderModal(props) {
   const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(props.open);
+  const renderButton = props.render;
+
   const handleClose = () => {
     setOpen(false);
     ctxDispatch({ type: "CART_CLEAR" });
@@ -32,125 +34,139 @@ export default function OrderModal(props) {
   });
 
   return (
-    <Dialog
-      open={open}
-      maxWidth={"lg"}
-      onClose={() => {
-        handleClose();
-        navigate("/");
-      }}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <div className="flex flex-col lg:w-max-[1150px] lg:w-[1150px] m-4 bg-[#f4f5f6] lg:bg-white">
-        {isLoading ? (
-          <div className="flex justify-center pt-36">
-            <CircularProgress color="inherit" />
-          </div>
-        ) : (
-          <div className="flex flex-col lg:flex-row">
-            <div className="flex flex-col p-4 lg:rounded shadow lg:w-[70%] h-fit lg:mr-4 bg-white">
-              <p className="py-4 border-b">
-                <strong className="text-lg">Order receipt</strong>
-              </p>
-              <strong className="text-lg py-4 border-b">ID: {props.id}</strong>
+    <>
+      {renderButton && (
+        <button
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className="bg-[#0e001a] hover:bg-[#0e001ac5] rounded text-white text-sm font-semibold py-2 px-3 transition"
+        >
+          Details
+        </button>
+      )}
+      <Dialog
+        open={open}
+        maxWidth={"lg"}
+        onClose={() => {
+          handleClose();
+          navigate(props.nav);
+        }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <div className="flex flex-col lg:w-max-[1150px] lg:w-[1150px] m-4 bg-[#f4f5f6] lg:bg-white">
+          {isLoading ? (
+            <div className="flex justify-center pt-36">
+              <CircularProgress color="inherit" />
+            </div>
+          ) : (
+            <div className="flex flex-col lg:flex-row">
+              <div className="flex flex-col p-4 lg:rounded shadow lg:w-[70%] h-fit lg:mr-4 bg-white">
+                <p className="py-4 border-b">
+                  <strong className="text-lg">Order receipt</strong>
+                </p>
+                <strong className="text-lg py-4 border-b">
+                  ID: {props.id}
+                </strong>
 
-              <div className="flex flex-col w-full">
-                <div className="placeOrderCont py-4 border-b">
-                  <div className="placeOrderHeader">
-                    <strong>Shipping</strong>
+                <div className="flex flex-col w-full">
+                  <div className="placeOrderCont py-4 border-b">
+                    <div className="placeOrderHeader">
+                      <strong>Shipping</strong>
+                    </div>
+                    <div>
+                      <strong>Name:</strong> {order.shippingAddress.fullName}.{" "}
+                      <br />
+                      <strong>Address: </strong> {order.shippingAddress.address}
+                      , {order.shippingAddress.city},{" "}
+                      {order.shippingAddress.postalCode},{" "}
+                      {order.shippingAddress.country}.
+                    </div>
                   </div>
-                  <div>
-                    <strong>Name:</strong> {order.shippingAddress.fullName}.{" "}
-                    <br />
-                    <strong>Address: </strong> {order.shippingAddress.address},{" "}
-                    {order.shippingAddress.city},{" "}
-                    {order.shippingAddress.postalCode},{" "}
-                    {order.shippingAddress.country}.
-                  </div>
-                </div>
 
-                <div className="placeOrderCont py-4 border-b">
-                  <div className="placeOrderHeader">
-                    <strong>Payment Method</strong>
+                  <div className="placeOrderCont py-4 border-b">
+                    <div className="placeOrderHeader">
+                      <strong>Payment Method</strong>
+                    </div>
+                    <div>
+                      <strong>Method:</strong> {order.paymentMethod}.
+                    </div>
                   </div>
-                  <div>
-                    <strong>Method:</strong> {order.paymentMethod}.
-                  </div>
-                </div>
 
-                <div className="placeOrderCont py-4 border-b">
-                  <div className="placeOrderHeader">
-                    <strong>Items</strong>
-                  </div>
-                  <div className="placeOrderItems text-sm">
-                    {order.orderItems.map((order) => (
-                      <div className="wholeOrderItem" key={order.id}>
-                        <div className="orderItemPart">
-                          <Link
-                            className="text-blue-400 underline"
-                            to={`/${order._id}`}
-                          >
-                            <div className="p-2">
-                              <img
-                                className="w-16 h-12 object-cover"
-                                src={order.image}
-                                alt={order.name}
-                              ></img>
-                            </div>
-                          </Link>
-                        </div>
-                        <div className="orderItemPart">
-                          <Link
-                            className="text-blue-400 underline"
-                            to={`/${order._id}`}
-                          >
-                            {order.name}
-                          </Link>
-                        </div>
-                        <div className="orderItemPart" key={order.id}>
-                          <div className="orderItemQuan">
-                            <span>{order.quantity}</span>
+                  <div className="placeOrderCont py-4 border-b">
+                    <div className="placeOrderHeader">
+                      <strong>Items</strong>
+                    </div>
+                    <div className="placeOrderItems text-sm">
+                      {order.orderItems.map((order) => (
+                        <div className="wholeOrderItem" key={order.id}>
+                          <div className="orderItemPart">
+                            <Link
+                              className="text-blue-400 underline"
+                              to={`/${order._id}`}
+                            >
+                              <div className="p-2">
+                                <img
+                                  className="w-16 h-12 object-cover"
+                                  src={order.image}
+                                  alt={order.name}
+                                ></img>
+                              </div>
+                            </Link>
                           </div>
+                          <div className="orderItemPart">
+                            <Link
+                              className="text-blue-400 underline"
+                              to={`/${order._id}`}
+                            >
+                              {order.name}
+                            </Link>
+                          </div>
+                          <div className="orderItemPart" key={order.id}>
+                            <div className="orderItemQuan">
+                              <span>{order.quantity}</span>
+                            </div>
+                          </div>
+                          <div className="orderItemPart">{order.price} EGP</div>
                         </div>
-                        <div className="orderItemPart">{order.price} EGP</div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
+                  </div>
+                  <div className="placeOrderCont py-4 border-b">
+                    <div className="placeOrderHeader">
+                      <strong>Order Summary</strong>
+                    </div>
+                    <div className="h3Padding">
+                      <strong>Items price</strong>
+                      <p>{order.itemsPrice.toFixed(2)} EGP</p>
+                      <strong>Shipping</strong>
+                      <p>{order.shippingPrice.toFixed(2)} EGP</p>
+                      <strong>Tax</strong>
+                      <p>{order.taxPrice.toFixed(2)} EGP</p>
+                    </div>
+                  </div>
+                  <div></div>
+                  <div className="flex flex-col py-4">
+                    <strong className="placeOrderHeader"> Order Total</strong>
+                    <strong>{order.totalPrice.toFixed(2)} EGP</strong>
                   </div>
                 </div>
-                <div className="placeOrderCont py-4 border-b">
-                  <div className="placeOrderHeader">
-                    <strong>Order Summary</strong>
-                  </div>
-                  <div className="h3Padding">
-                    <strong>Items price</strong>
-                    <p>{order.itemsPrice.toFixed(2)} EGP</p>
-                    <strong>Shipping</strong>
-                    <p>{order.shippingPrice.toFixed(2)} EGP</p>
-                    <strong>Tax</strong>
-                    <p>{order.taxPrice.toFixed(2)} EGP</p>
-                  </div>
-                </div>
-                <div></div>
-                <div className="flex flex-col py-4">
-                  <strong className="placeOrderHeader"> Order Total</strong>
-                  <strong>{order.totalPrice.toFixed(2)} EGP</strong>
+              </div>
+              <div className="hidden lg:flex flex-col p-4 rounded shadow lg:w-[30%] h-fit items-center text-center text-lg font-semibold">
+                <span className="border-4 border-solid border-[#009f29] text-[#009f29] rounded-[50%] w-[56px] h-[56px] flex items-center justify-center mb-4 ">
+                  <MdDone size={40} />
+                </span>
+                <div>
+                  Thank You <br /> your order successfully
+                  <br />
+                  been placed.
                 </div>
               </div>
             </div>
-            <div className="hidden lg:flex flex-col p-4 rounded shadow lg:w-[30%] h-fit items-center text-center text-lg font-semibold">
-              <span className="border-4 border-solid border-[#009f29] text-[#009f29] rounded-[50%] w-[56px] h-[56px] flex items-center justify-center mb-4 ">
-                <MdDone size={40} />
-              </span>
-              <div>
-                Thank You <br /> your order successfully
-                <br />
-                been placed.
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </Dialog>
+          )}
+        </div>
+      </Dialog>
+    </>
   );
 }
