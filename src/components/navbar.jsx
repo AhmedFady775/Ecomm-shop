7a4cca89903760239d8e9f891d7f2482dc4e727d
Ahmed Mohamed Fady <fadiahmed298@gmail.com>
2023-04-09 07:25:34 +0200

    animations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import { Link } from "react-router-dom";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { Drawer } from "@mui/material";
 import { Store } from "../redux/Store";
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
@@ -21,6 +21,15 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Badge from "@mui/material/Badge";
 
+import { BsPersonGear } from "react-icons/bs";
+import { BsBoxSeam } from "react-icons/bs";
+import { GoCreditCard } from "react-icons/go";
+import { FiRefreshCw } from "react-icons/fi";
+import {
+  MdKeyboardArrowDown,
+  MdOutlineAdminPanelSettings,
+} from "react-icons/md";
+
 import { AiFillCreditCard } from "react-icons/ai";
 import { MdAssignmentInd } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
@@ -30,36 +39,25 @@ import { BsBox } from "react-icons/bs";
 import { BsCart3 } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 import { BsTelephone } from "react-icons/bs";
+import Dropdown from "./Dropdown";
+import DropDownList from "./DropDownList";
+import { userInfoStore } from "../suztand/Store";
 
 function Navbar() {
+  let menuRef = useRef();
+
   const { state } = useContext(Store);
-  const { cart, userInfo } = state;
+  const { cart } = state;
+  const userInfo = userInfoStore((state) => state.userInfo);
+  const UserSignOut = userInfoStore((state) => state.userSignOut);
+
   const { dispatch: ctxDispatch } = useContext(Store);
 
-  const [anchorEl, setAnchorEl] = useState(false);
-  const [anchorEl1, setAnchorEl1] = useState(false);
-
-  const openDrop = anchorEl;
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const openDrop1 = anchorEl1;
-  const handleClick1 = (event) => {
-    setAnchorEl1(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(false);
-  };
-
-  const handleClose1 = () => {
-    setAnchorEl1(null);
-  };
-
+  const [openCategoriesMenu, setopenCategoriesMenu] = useState(false);
+  const [openUserMenu, setopenUserMenu] = useState(false);
   //signout
   const signoutHandler = () => {
-    ctxDispatch({ type: "USER_SIGNOUT" });
+    UserSignOut();
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress");
     localStorage.removeItem("paymentMethod");
@@ -70,11 +68,64 @@ function Navbar() {
     setOpen(newOpen);
   };
 
-  const CATEGORIES = [
-    { link: "/category/camera", name: "Cameras" },
-    { link: "/category/wire", name: "Wires" },
-    { link: "/category/device", name: "Devices" },
+  const categoriesList = [
+    {
+      link: "/category/camera",
+      name: "Cameras",
+      image:
+        "https://i1.adis.ws/i/canon/Dome-camera_960x1080-b06d4418-8d22-11eb-ad11-b083fe5731f9?w=720&qlt=90&fmt=jpg&fmt.options=interlaced&bg=rgb(255,255,255)",
+    },
+    {
+      link: "/category/wire",
+      name: "Wires",
+      image:
+        "https://5.imimg.com/data5/FC/BP/MY-18879959/flexible-wire-500x500.jpg",
+    },
+    {
+      link: "/category/device",
+      name: "Devices",
+      image:
+        "https://www.dseinternational.com/images/librariesprovider8/counter-surveillance-tscm-equipment/vosker.jpg",
+    },
   ];
+
+  const userList = [
+    {
+      link: "/customer/account",
+      name: "Account",
+      icon: <BsPersonGear size={25} />,
+    },
+    {
+      link: "/customer/myorders",
+      name: "Orders",
+      icon: <BsBoxSeam size={22} />,
+    },
+    {
+      link: "/customer/payments",
+      name: "Payments",
+      icon: <GoCreditCard size={22} />,
+    },
+    {
+      link: "/customer/returns",
+      name: "Returns",
+      icon: <FiRefreshCw size={22} />,
+    },
+  ];
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setopenUserMenu(false);
+        setopenCategoriesMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, []);
 
   const list = () => (
     <div className="w-[80vw] h-full bg-[f7f7f7]">
@@ -175,7 +226,7 @@ function Navbar() {
             </li>
           </Link>
 
-          {CATEGORIES.map((category, i) => (
+          {categoriesList.map((category, i) => (
             <Link key={i} onClick={toggleDrawer(false)} to={category.link}>
               <li className="nav-item border-b-2 border-gray-100">
                 {category.name}
@@ -199,7 +250,7 @@ function Navbar() {
 
   return (
     <div className="sticky top-0 z-50">
-      <nav className="hidden lg:flex bg-[#0e001a] text-white h-10 ">
+      <nav className="hidden lg:flex bg-[#0e001a] text-white h-10">
         <ul className="h-full flex flex-row items-center justify-between lg:w-max-[1184px] lg:w-[1184px] lg:m-auto text-xs">
           <ul className="flex flex-row space-x-4">
             <li>AR</li>
@@ -210,7 +261,11 @@ function Navbar() {
           <li>Call *****</li>
         </ul>
       </nav>
-      <nav className="h-14 lg:h-20 bg-white text-black shadow-sm ">
+      <nav
+        className={`h-14 lg:h-20 bg-white text-black ${
+          openCategoriesMenu ? " " : "shadow-sm"
+        }`}
+      >
         <section className="flex flex-row items-center justify-between h-full lg:w-max-[1184px] lg:w-[1184px] lg:m-auto px-3 lg:px-0">
           <span className="flex lg:hidden">
             <MenuIcon onClick={toggleDrawer(true)} fontSize="large" />
@@ -222,37 +277,43 @@ function Navbar() {
             <Link className="flex mx-4 lg:mx-0" to="/">
               <p className="text-2xl md:text-3xl font-bold Robot">V2S</p>
             </Link>
-            <div
-              onClick={handleClick1}
-              className="hidden lg:flex flex-row ml-10 font-semibold cursor-pointer hover:bg-[#f4f5f6] p-3 rounded-md"
-            >
-              Categories <KeyboardArrowDown />
+            <div className="hidden lg:flex flex-col ml-10" ref={menuRef}>
+              <button
+                className="cursor-pointer flex items-center p-3 hover:bg-[#f4f5f6] transition rounded-lg font-semibold"
+                onMouseEnter={() => setopenCategoriesMenu(!openCategoriesMenu)}
+              >
+                Categories <RiArrowDownSLine size={22} />
+              </button>
+              <DropDownList open={openCategoriesMenu}>
+                <ul
+                  onMouseLeave={() => setopenCategoriesMenu(false)}
+                  className="flex flex-col"
+                >
+                  {categoriesList.map((category, i) => (
+                    <Link
+                      to={category.link}
+                      className={`hover:bg-[#f4f5f6] transition ${
+                        categoriesList.length - 1 === i ? "rounded-b-lg" : " "
+                      }`}
+                    >
+                      <li
+                        className={`flex items-center ${
+                          categoriesList.length - 1 === i ? "" : "border-b"
+                        } w-full p-3`}
+                      >
+                        <div className="flex w-8 h-8 mr-4">
+                          <img
+                            className="w-full h-full object-cover"
+                            src={category.image}
+                          />
+                        </div>
+                        {category.name}
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </DropDownList>
             </div>
-            <Menu
-              anchorEl={anchorEl1}
-              open={openDrop1}
-              onClose={handleClose1}
-              PaperProps={{
-                elevation: 3,
-                sx: {
-                  width: 180,
-                  overflow: "visible",
-                  mt: 0,
-                },
-              }}
-            >
-              <Link onClick={handleClose1} to="/category/camera">
-                <MenuItem sx={{ py: 1.5 }}>Cameras</MenuItem>
-              </Link>
-
-              <Link onClick={handleClose1} to="/category/wire">
-                <MenuItem sx={{ py: 1.5 }}>Wires</MenuItem>
-              </Link>
-
-              <Link onClick={handleClose1} to="/category/device">
-                <MenuItem sx={{ py: 1.5 }}>Devices</MenuItem>
-              </Link>
-            </Menu>
           </div>
           <div className="flex flex-row justify-end w-full lg:w-[50%] items-center h-12 text-[#7f8286] relative">
             <input
@@ -263,112 +324,59 @@ function Navbar() {
           </div>
           <div className="flex flex-row items-center lg:gap-8">
             {userInfo ? (
-              <div className="cursor-pointer flex ">
-                <div className="flex flex-row">
-                  <div
-                    onClick={handleClick}
-                    className="hidden lg:flex flex-row items-center gap-2 text-sm"
-                  >
-                    <RxPerson size={25} />
-                    <span className="hidden lg:flex text-sm font-normal leading-5 tracking-[0]">
-                      Hello, {userInfo.firstName}.
-                    </span>
-                  </div>
-                  <Menu
-                    anchorEl={anchorEl}
-                    open={openDrop}
-                    onClose={handleClose}
-                    PaperProps={{
-                      elevation: 0,
-                      sx: {
-                        width: 200,
-                        overflow: "visible",
-                        filter: "drop-shadow(0px 2px 2px rgba(0,0,0,0.32))",
-                        mt: 2,
-                        "&:before": {
-                          content: '""',
-                          display: "block",
-                          position: "absolute",
-                          top: 0,
-                          right: 10,
-                          width: 10,
-                          height: 10,
-                          bgcolor: "background.paper",
-                          transform: "translateY(-50%) rotate(45deg)",
-                          zIndex: 0,
-                        },
-                      },
-                    }}
-                    transformOrigin={{
-                      horizontal: "right",
-                      vertical: "top",
-                    }}
-                    anchorOrigin={{
-                      horizontal: "right",
-                      vertical: "bottom",
-                    }}
-                  >
-                    <Link onClick={handleClose} to="/customer/account">
-                      <MenuItem sx={{ py: 1.5 }}>
-                        <ListItemIcon>
-                          <PersonIcon fontSize="small" />
-                        </ListItemIcon>
-                        Profile
-                      </MenuItem>
-                    </Link>
-
-                    <Link onClick={handleClose} to="/customer/myorders">
-                      <MenuItem sx={{ py: 1.5 }}>
-                        <ListItemIcon>
-                          <AssignmentIcon fontSize="small" />
-                        </ListItemIcon>
-                        Orders
-                      </MenuItem>
-                    </Link>
-
-                    <Link onClick={handleClose} to="/customer/payments">
-                      <MenuItem sx={{ py: 1.5 }}>
-                        <ListItemIcon>
-                          <CreditCardIcon fontSize="small" />
-                        </ListItemIcon>
-                        Payments
-                      </MenuItem>
-                    </Link>
-
-                    <Link onClick={handleClose} to="/customer/returns">
-                      <MenuItem sx={{ py: 1.5 }}>
-                        <ListItemIcon>
-                          <AssignmentReturnIcon fontSize="small" />
-                        </ListItemIcon>
-                        Returns
-                      </MenuItem>
-                    </Link>
-
-                    <Divider />
-
-                    {userInfo && userInfo.isAdmin ? (
-                      <Link onClick={handleClose} to="/admin/dashboard">
-                        <MenuItem sx={{ py: 1.5 }}>
-                          <ListItemIcon>
-                            <AdminPanelSettingsIcon fontSize="small" />
-                          </ListItemIcon>
-                          Admin
-                        </MenuItem>
-                      </Link>
-                    ) : null}
-
-                    <Divider />
-
-                    <Link to="/login" onClick={signoutHandler}>
-                      <MenuItem>
-                        <ListItemIcon>
-                          <Logout fontSize="small" />
-                        </ListItemIcon>
-                        Logout
-                      </MenuItem>
-                    </Link>
-                  </Menu>
+              <div className="flex flex-col" ref={menuRef}>
+                <div
+                  onClick={() => setopenUserMenu(!openUserMenu)}
+                  className="hidden lg:flex flex-row items-center gap-2 text-sm cursor-pointer"
+                >
+                  <RxPerson size={25} />
+                  <span className="hidden lg:flex text-sm font-normal leading-5 tracking-[0]">
+                    Hello, {userInfo.firstName}.
+                  </span>
                 </div>
+                <Dropdown open={openUserMenu} anchorRight>
+                  <ul
+                    className="flex flex-col"
+                    onClick={() => {
+                      console.log("clicked");
+                      setopenUserMenu(false);
+                    }}
+                  >
+                    {userList.map((user) => (
+                      <Link
+                        to={user.link}
+                        className="hover:bg-[#f4f5f6] z-50 transition"
+                      >
+                        <li className="flex items-center w-full px-4 py-3">
+                          <div className="flex-4 mr-4">{user.icon}</div>
+                          {user.name}
+                        </li>
+                      </Link>
+                    ))}
+                    {userInfo.isAdmin && (
+                      <Link
+                        to="/admin/dashboard"
+                        className="hover:bg-[#f4f5f6] border-y"
+                      >
+                        <li className="flex items-center w-full px-4 py-3">
+                          <div className="flex-4 mr-4">
+                            <MdOutlineAdminPanelSettings size={25} />
+                          </div>
+                          Admin
+                        </li>
+                      </Link>
+                    )}
+                    <Link
+                      to="/login"
+                      onClick={signoutHandler}
+                      className="hover:bg-[#f4f5f6]"
+                    >
+                      <li className="flex text-sm items-center w-full px-4 py-3 text-[#0066be]">
+                        Log out
+                      </li>
+                    </Link>
+                  </ul>
+                </Dropdown>
               </div>
             ) : (
               <Link
